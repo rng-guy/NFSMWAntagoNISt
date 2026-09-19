0be1074168bb037ba47ad9088ed96a0c8ede393e
rng-guy <dcim.koor@gmail.com>
2026-09-19 21:36:01 +0200

    Rearranged functions.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -165,7 +165,7 @@ ASSEMBLY_DETOUR(SceneVehicles, 0x6F30CB, 0x6F30D1)
 
 
 
-// Initialisation helpers ---------------------------------------------------------------------------------------------------------------------------
+// Hashing function ---------------------------------------------------------------------------------------------------------------------------------
 
 [[nodiscard]] static constexpr vault GetVaultHash(std::string_view input)
 {
@@ -240,16 +240,20 @@ ASSEMBLY_DETOUR(SceneVehicles, 0x6F30CB, 0x6F30D1)
 
 
 
+
+
+// Initialisation helpers ---------------------------------------------------------------------------------------------------------------------------
+
 [[nodiscard]] static bool IsValidVehicleType(const vault type)
 {
-	const auto GetVaultNode          = AsFunction<address __cdecl    (vault, vault)>          (0x455FD0);
+	const auto GetVaultNode          = AsFunction<address __cdecl    (vault,   vault)>        (0x455FD0);
 	const auto GetVaultNodeAttribute = AsFunction<address __thiscall (address, vault, size_t)>(0x454190);
 
 	const address node = GetVaultNode("pvehicle"_vlt, type);
 	if (not node) return false; // unknown attribute node
 
-	const address attribute = GetVaultNodeAttribute(node, "CLASS"_vlt, 0);
-	if (not attribute) return false; // missing "CLASS" attribute
+	const address attribute = GetVaultNodeAttribute(node, "CLASS"_vlt, /* index = */ 0);
+	if (not attribute) return false; // missing "CLASS" attribute parameter
 
 	switch (AsReference<vault>(attribute + 0x8))
 	{
